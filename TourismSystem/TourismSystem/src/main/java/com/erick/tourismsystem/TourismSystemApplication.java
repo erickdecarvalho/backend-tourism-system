e4f88fe221f9628b177f43027e71da9aa86362e1
erick7amorim <erickamorimcarv@gmail.com>
2024-09-08 14:30:08 -0300

    backend project created

---
+++ TourismSystem/TourismSystem/src/main/java/com/erick/tourismsystem/TourismSystemApplication.java
@@ -0,0 +1,13 @@
+package com.erick.tourismsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourismSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourismSystemApplication.class, args);
+	}
+
+}
---
+++ TourismSystem/TourismSystem/src/test/java/com/erick/tourismsystem/TourismSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erick.tourismsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourismSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
